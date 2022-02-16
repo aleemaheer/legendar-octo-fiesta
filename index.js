42c55e639b6d50d21afa5048ba24937bc8ef9d02
aleem aheer <aleemaheer5769644@gmail.com>
2022-02-17 00:22:07 +0500

    Updated

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const fs = require('fs');
-
 // Reading a json file
 const read_directory_path = path.join(__dirname, 'public', 'data');
 const files = fs.readdirSync(read_directory_path);
@@ -11,26 +10,21 @@ const data = JSON.parse(
 return {
     name: file.split(' : ')[0]
 }
-
 */
-
 const profiles = files.map((file) => {
     const data = JSON.parse(
       fs.readFileSync(`${path.join(__dirname, 'public', 'data', file)}`, 'utf8'),
     )
     return {
-      name: file.split(' : ')[0],
+      name: file.split(':    ')[0],
       ...data,
     }
   })
-
 // generate list file
-
 const output = profiles.map((profile) => ({
     name: profile.name,
-    gender: profile.gender
+    gender: profile.gender,
+    age: profile.age
 }))
-
 //fs.writeFileSync(writeDirectoryPath, JSON.stringify(output))
-
 console.log(output);
