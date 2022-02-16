effb3626cdbb0c9ccaa090701daa7f3d6b69a414
aleem aheer <aleemaheer5769644@gmail.com>
2022-02-17 00:10:32 +0500

    Successfully reading json files and printing their data in console

--- index.js
+++ index.js
@@ -2,3 +2,35 @@ const path = require('path');
 const fs = require('fs');
 
 // Reading a json file
+const read_directory_path = path.join(__dirname, 'public', 'data');
+const files = fs.readdirSync(read_directory_path);
+/*
+const data = JSON.parse(
+    fs.readFileSync(`${path.join(__dirname, 'public', 'data', file)}`, 'utf-8'),
+)
+return {
+    name: file.split(' : ')[0]
+}
+
+*/
+
+const profiles = files.map((file) => {
+    const data = JSON.parse(
+      fs.readFileSync(`${path.join(__dirname, 'public', 'data', file)}`, 'utf8'),
+    )
+    return {
+      name: file.split(' : ')[0],
+      ...data,
+    }
+  })
+
+// generate list file
+
+const output = profiles.map((profile) => ({
+    name: profile.name,
+    gender: profile.gender
+}))
+
+//fs.writeFileSync(writeDirectoryPath, JSON.stringify(output))
+
+console.log(output);
