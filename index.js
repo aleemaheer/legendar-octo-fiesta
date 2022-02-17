17569a3063e7c7f7f18132d9d2a4f58b8b0732d3
aleem aheer <aleemaheer5769644@gmail.com>
2022-02-17 23:03:58 +0500

    Updated

--- index.js
+++ index.js
@@ -3,14 +3,6 @@ const fs = require('fs');
 // Reading a json file
 const read_directory_path = path.join(__dirname, 'public', 'data');
 const files = fs.readdirSync(read_directory_path);
-/*
-const data = JSON.parse(
-    fs.readFileSync(`${path.join(__dirname, 'public', 'data', file)}`, 'utf-8'),
-)
-return {
-    name: file.split(' : ')[0]
-}
-*/
 const profiles = files.map((file) => {
     const data = JSON.parse(
       fs.readFileSync(`${path.join(__dirname, 'public', 'data', file)}`, 'utf8'),
